5588bb4f07af843db389d36ea555dd82d846aedf
Pranav <pranavkanire@gmail.com>
2022-07-11 22:34:33 +0530

    practice

--- leetcode/TrappingRainWater.cpp
+++ leetcode/TrappingRainWater.cpp
@@ -1,26 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int trap(vector<int>& height) {
+        /*
         int n = height.size();
+        vector<int> left(n,0);
         
-        int left_max=0, right_max=0;
-        int left=0,right=n-1,wtrap=0;
-        while(left<right){
-            if(height[left] < height[right]){
-                if(height[left] >= left_max)
-                    left_max = height[left];
-                else
-                    wtrap+=(left_max-height[left]);
-                left++;
-            }
-            else{
-                if(height[right] >= right_max)
-                    right_max = height[right];
-                else
-                    wtrap+=(right_max-height[right]);
-                right--;
+        // get previous greatest height
+        int l = height[0];
+        for(int i=1;i<n;i++) {
+            left[i] = l;
+            l = max(l,height[i]);
+        }
+        
+        // calculate next greatest element
+        // also update the water trapped as min(next and prev greatest)
+        // water can be trapped only if both boundaries are higher than its height
+        int r = height[n-1], water = 0, t;
+        for(int i=n-2;i>0;i--) {
+            t = min(r, left[i]);
+            water += t>height[i] ? t-height[i] : 0;
+            r = max(r, height[i]);
+        }
+        
+        return water;
+        */
+        int i = 0;
+        int j = height.size() - 1;
+        
+        int maxLeft = height[i];
+        int maxRight = height[j];
+        
+        int result = 0;
+        
+        while (i < j) {
+            if (maxLeft <= maxRight) {
+                i++;
+                maxLeft = max(maxLeft, height[i]);
+                result += maxLeft - height[i];
+            } else {
+                j--;
+                maxRight = max(maxRight, height[j]);
+                result += maxRight - height[j];
             }
         }
-        return wtrap;
+        
+        return result;
     }
 };
+
+int main() {
+    Solution a;
+    vector<int> h = {0,1,0,2,1,0,1,3,2,1,2,1};
+    // {0,1,0,2,1,0,1,3,2,1,2,1}
+    // {4,2,0,3,2,5}
+    cout << a.trap(h) << endl;
+    return 0;
+}
